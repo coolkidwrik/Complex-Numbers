4be057f6e2e033532903e4f04c23243d2b4a6f55
coolkidwrik <wriksen@students.cs.ubc.ca address>
2023-05-27 08:31:16 +0800

    added change were user can chain together a list of commands with two inputs

--- ui/calculator.py
+++ ui/calculator.py
@@ -312,6 +312,8 @@ def equal_lambda(text: Entry, press_down_buttons: dict):
 def double_input_function_lambda(s: str, text: Entry, press_down_buttons: dict):
     if text.get() == "":
         return
+    elif text.get() != "" and len(inputs) == 2:
+        equal_lambda(text, press_down_buttons)
     else:
         button = press_down_buttons[s]
         button['state'] = DISABLED
